d107f4b440e9f524f845a84e4909ec3adfc926b2
ilkaypolat1985 <ilkaypolat1985@gmail.com>
2020-01-01 11:42:53 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculus calculus = new Calculus();
+
+        System.out.println(calculus.sum(3, 4));
+
+        System.out.println(calculus.sum(3, 4, 5));
+    }
+}
